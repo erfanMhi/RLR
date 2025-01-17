0c8d41bd86332484413cbba2955b1617e738faa5
ErfanMhi <mhi.erfan1@gmail.com>
2025-01-17 18:02:32 -0500

    feat: add initial temporary RLR framework files for recognizing this as a package.

---
+++ rlr/__init__.py
@@ -0,0 +1,42 @@
+# """RLR: Reinforcement Learning for Reasoning - A PyTorch-based RL Framework."""
+
+# from rlr.env.base import LLMReasoningEnv
+# from rlr.policy.base import IPolicy, ReasoningPolicy
+# from rlr.buffer.replay_buffer import ReplayBuffer
+# from rlr.trainer.trainer import RLTrainer
+# from rlr.planner.planner import IPlanner, SimplePlanner
+# from rlr.world_model.world_model import IWorldModel, LearnedWorldModel
+# from rlr.utils.config import Config, TrainingConfig, ModelConfig, LoggingConfig
+# from rlr.utils.logger import Logger
+
+# __version__ = "0.1.0"
+
+# __all__ = [
+#     # Environment
+#     "LLMReasoningEnv",
+
+#     # Policy
+#     "IPolicy",
+#     "ReasoningPolicy",
+
+#     # Buffer
+#     "ReplayBuffer",
+
+#     # Trainer
+#     "RLTrainer",
+
+#     # Planner
+#     "IPlanner",
+#     "SimplePlanner",
+
+#     # World Model
+#     "IWorldModel",
+#     "LearnedWorldModel",
+
+#     # Utils
+#     "Config",
+#     "TrainingConfig",
+#     "ModelConfig",
+#     "LoggingConfig",
+#     "Logger",
+# ]
